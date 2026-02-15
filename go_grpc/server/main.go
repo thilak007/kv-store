e6280936bc4ee4f01524756df9d7722364d5fae4
Thilak Raj Murugan <mthilakraj96@gmail.com>
2026-02-14 20:12:25 -0600

    Add request ID

--- go_grpc/server/main.go
+++ go_grpc/server/main.go
@@ -8,14 +8,16 @@ import (
 	"net"
 	"sort"
 	"sync"
+	"sync/atomic"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/peer"
 )
 
 var (
-	records = make(map[string]string)
-	mu      sync.RWMutex
+	records   = make(map[string]string)
+	mu        sync.RWMutex
+	requestID uint64
 )
 
 type server struct {
@@ -23,13 +25,14 @@ type server struct {
 }
 
 func (s *server) Put(ctx context.Context, in *pb.PutRequest) (*pb.PutResponse, error) {
+	reqID := atomic.AddUint64(&requestID, 1)
 	mu.Lock()
 	defer mu.Unlock()
 
 	p, ok := peer.FromContext(ctx)
 
 	if ok {
-		log.Printf("Received PUT from %s for key: %s and value: %s", p.Addr.String(), in.Key, in.Value)
+		log.Printf("[ReqID: %d] Received PUT from %s for key: %s and value: %s", reqID, p.Addr.String(), in.Key, in.Value)
 	}
 
 	key := in.Key
@@ -37,7 +40,7 @@ func (s *server) Put(ctx context.Context, in *pb.PutRequest) (*pb.PutResponse, e
 	_, exists := records[key]
 
 	records[key] = value
-	log.Printf("Sent PUT from %s for key: %s and value: %s. AlreadyExists: %t", p.Addr.String(), in.Key, in.Value, exists)
+	log.Printf("[ReqID: %d] Sent PUT from %s for key: %s and value: %s. AlreadyExists: %t", reqID, p.Addr.String(), in.Key, in.Value, exists)
 
 	return &pb.PutResponse{
 		AlreadyExists: exists,
@@ -45,12 +48,12 @@ func (s *server) Put(ctx context.Context, in *pb.PutRequest) (*pb.PutResponse, e
 }
 
 func (s *server) Swap(ctx context.Context, in *pb.SwapRequest) (*pb.SwapResponse, error) {
-
+	reqID := atomic.AddUint64(&requestID, 1)
 	mu.Lock()
 	defer mu.Unlock()
 	p, ok := peer.FromContext(ctx)
 	if ok {
-		log.Printf("Received SWAP from %s for key: %s and new value: %s", p.Addr.String(), in.Key, in.Value)
+		log.Printf("[ReqID: %d] Received SWAP from %s for key: %s and new value: %s", reqID, p.Addr.String(), in.Key, in.Value)
 	}
 
 	key := in.Key
@@ -61,7 +64,7 @@ func (s *server) Swap(ctx context.Context, in *pb.SwapRequest) (*pb.SwapResponse
 		records[key] = newvalue
 	}
 
-	log.Printf("Sent SWAP from %s for key: %s. OldValue: %s changed to NewValue: %s", p.Addr.String(), in.Key, oldvalue, newvalue)
+	log.Printf("[ReqID: %d] Sent SWAP from %s for key: %s. OldValue: %s changed to NewValue: %s", reqID, p.Addr.String(), in.Key, oldvalue, newvalue)
 
 	return &pb.SwapResponse{
 		OldValue: oldvalue,
@@ -70,17 +73,18 @@ func (s *server) Swap(ctx context.Context, in *pb.SwapRequest) (*pb.SwapResponse
 }
 
 func (s *server) Get(ctx context.Context, in *pb.GetRequest) (*pb.GetResponse, error) {
+	reqID := atomic.AddUint64(&requestID, 1)
 	mu.Lock()
 	defer mu.Unlock()
 	p, ok := peer.FromContext(ctx)
 	if ok {
-		log.Printf("Received GET from %s for key: %s", p.Addr.String(), in.Key)
+		log.Printf("[ReqID: %d] Received GET from %s for key: %s", reqID, p.Addr.String(), in.Key)
 	}
 
 	key := in.Key
 	value, exists := records[key]
 
-	log.Printf("Sent GET from %s for key: %s. Got value: %s, exists: %t", p.Addr.String(), in.Key, value, exists)
+	log.Printf("[ReqID: %d] Sent GET from %s for key: %s. Got value: %s, exists: %t", reqID, p.Addr.String(), in.Key, value, exists)
 
 	return &pb.GetResponse{
 		Value:  value,
@@ -89,10 +93,11 @@ func (s *server) Get(ctx context.Context, in *pb.GetRequest) (*pb.GetResponse, e
 }
 
 func (s *server) Scan(in *pb.ScanRequest, stream pb.KVService_ScanServer) error {
+	reqID := atomic.AddUint64(&requestID, 1)
 	mu.Lock()
 	p, ok := peer.FromContext(stream.Context())
 	if ok {
-		log.Printf("Received SCAN from %s from key: %s to key: %s", p.Addr.String(), in.StartKey, in.EndKey)
+		log.Printf("[ReqID: %d] Received SCAN from %s from key: %s to key: %s", reqID, p.Addr.String(), in.StartKey, in.EndKey)
 	}
 
 	startKey := in.StartKey
@@ -113,7 +118,7 @@ func (s *server) Scan(in *pb.ScanRequest, stream pb.KVService_ScanServer) error 
 			Key:   key,
 			Value: snapshot[key],
 		}
-		log.Printf("Sent SCAN from %s from key: %s to key: %s. Key: %s, Value: %s", p.Addr.String(), in.StartKey, in.EndKey, key, snapshot[key])
+		log.Printf("[ReqID: %d] Sent SCAN from %s from key: %s to key: %s. Key: %s, Value: %s", reqID, p.Addr.String(), in.StartKey, in.EndKey, key, snapshot[key])
 		if err := stream.Send(ScanRes); err != nil {
 			return err
 		}
@@ -123,11 +128,12 @@ func (s *server) Scan(in *pb.ScanRequest, stream pb.KVService_ScanServer) error 
 }
 
 func (s *server) Delete(ctx context.Context, in *pb.DeleteRequest) (*pb.DeleteResponse, error) {
+	reqID := atomic.AddUint64(&requestID, 1)
 	mu.Lock()
 	defer mu.Unlock()
 	p, ok := peer.FromContext(ctx)
 	if ok {
-		log.Printf("Received DELETE from %s for key: %s", p.Addr.String(), in.Key)
+		log.Printf("[ReqID: %d] Received DELETE from %s for key: %s", reqID, p.Addr.String(), in.Key)
 	}
 
 	key := in.Key
@@ -136,7 +142,7 @@ func (s *server) Delete(ctx context.Context, in *pb.DeleteRequest) (*pb.DeleteRe
 		delete(records, key)
 	}
 
-	log.Printf("Sent DELETE from %s for key: %s. Exists: %t", p.Addr.String(), in.Key, exists)
+	log.Printf("[ReqID: %d] Sent DELETE from %s for key: %s. Exists: %t", reqID, p.Addr.String(), in.Key, exists)
 
 	return &pb.DeleteResponse{
 		Exists: exists,
