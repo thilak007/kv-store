1e48aa7c915f84c51dc1615ec2d2392b59fa5bba
Thilak Raj Murugan <mthilakraj96@gmail.com>
2026-02-15 13:43:35 -0600

    Setup necessary packages for cloudlab

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# uv manager
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+
+# Run these inside the project directory
+uv python install 3.12
+uv sync
+
+# apt install packages
+sudo apt update
+sudo apt install tree just default-jre liblog4j2-java
+
