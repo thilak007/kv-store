3c88ecc6a8bb7dcd86adc3026fa426cb9af1e591
Thilak Raj Murugan <mthilakraj96@gmail.com>
2026-03-03 15:25:03 +0000

    Add test commands in clouldlab

--- demo_commands.sh
+++ demo_commands.sh
@@ -1,3 +1,4 @@
+# p1 Testing in local
 just p1::test1
 
 just p1::test4
@@ -7,3 +8,17 @@ just p1::fuzz 1 "yes" "127.0.0.1:3777"
 
 just p1::fuzz 2 "no" "127.0.0.1:3777"
 just p1::fuzz 2 "yes" "127.0.0.1:3777"
+
+# p2 testing in Clouldlab
+
+# Server & Manager IP - 10.10.1.2
+
+### Client side :
+just p2::fuzz "2" "no" "10.10.1.2:3666"
+##################################################################
+
+### Server side :
+just p2::setupcluster 2 10.10.1.2
+
+# Note - Remote disk file after each run of server
+just p2::kill
