b34642588d2b77b9f724adcc58a8c42e570b4ee2
Thilak Raj Murugan <mthilakraj96@gmail.com>
2026-04-13 14:48:57 -0500

    Increase heartbest timer to 250ms

--- go_grpc/raft/replication.go
+++ go_grpc/raft/replication.go
@@ -42,9 +42,9 @@ func (rf *RaftNode) ProposeCmd(cmd *Command) (uint64, error) {
 // 1. Replicates new log entries to followers when signaled via replicateCh
 // 2. Sends periodic heartbeats to prevent follower election timeouts
 //
-// Heartbeat interval: 100ms (must be < minimum election timeout of 500ms)
+// Heartbeat interval: 250ms (must be < minimum election timeout of 500ms)
 func (rf *RaftNode) replicateAndHeartbeatLoop() {
-	const heartbeatInterval = 100 * time.Millisecond
+	const heartbeatInterval = 250 * time.Millisecond
 	ticker := time.NewTicker(heartbeatInterval)
 	defer ticker.Stop()
 
