b32f8d51eb4796544dd1fe5a2ccbb51e3c05858b
Thilak Raj Murugan <mthilakraj96@gmail.com>
2026-05-05 21:35:41 -0500

    Update yscb benchmark script

--- demo/ycsb_demo.sh
+++ demo/ycsb_demo.sh
@@ -175,10 +175,10 @@ echo "Starting Fuzz Test Suite"
 echo "========================================="
 
 # 1.  Fuzz 5 servers, healthy
-# run_fuzz_test 5 no
+run_fuzz_test 5 no
 
 # 2.  Fuzz 5 servers, crashing
-# run_fuzz_test 5 yes
+run_fuzz_test 5 yes
 
 echo ""
 echo "========================================="
@@ -193,28 +193,20 @@ echo "========================================="
 echo "Starting YCSB Test Suite"
 echo "========================================="
 
-run_ycsb_test 10 f 3
-
 # # 1) 10 clients on workloads A-F for RF in {1,3,5}
-# for workload in a b c d e f; do
-#     for rf in 1 3 5; do
-#         run_ycsb_test 10 "${workload}" "${rf}"
-#     done
-# done
+for workload in a b c d e f; do
+    for rf in 1 3 5; do
+        run_ycsb_test 10 "${workload}" "${rf}"
+    done
+done
 
 # 2) Workload A, scale clients from 1, 20, 30 for RF in {1,5}
 # Chosen client counts to represent the 1->30 scaling curve.
-# for nclis in 20; do
-#     for rf in 5; do
-#         run_ycsb_test "${nclis}" a "${rf}"
-#     done
-# done
-
-# for nclis in 30; do
-#     for rf in 1 5; do
-#         run_ycsb_test "${nclis}" a "${rf}"
-#     done
-# done
+for nclis in 1 20 30; do
+    for rf in 1 5; do
+        run_ycsb_test "${nclis}" a "${rf}"
+    done
+done
 
 echo ""
 echo "========================================="
